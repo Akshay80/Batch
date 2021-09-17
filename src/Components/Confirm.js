adb743d38b9887c26a12f0ad38dd801195b108b7
Akshay Bisht <akshay.bisht@ftechiz.com>
2021-09-17 18:15:51 +0530

    copy feature added

--- src/Components/Confirm.js
+++ src/Components/Confirm.js
@@ -1,8 +1,12 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import { Redirect } from "react-router-dom";
 
 import DashboardContext from "../context/dashboard/DashboardContext";
+import copy from '../images/copy_icon.png'; 
+import copy1 from '../images/copied_icon.png'; 
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import QRCode from "react-qr-code";
 
 import "../css/confirm.css";
 
@@ -14,7 +18,13 @@ const Confirm = (props) => {
     confirmPayment,
     confirmPaymentData,
   } = useContext(DashboardContext);
+  const [isCopied, setisCopied] = useState(false);
 
+
+  const user = JSON.parse(localStorage.getItem("user"));
+
+
+  console.log("MY BTC ADDRESS: ", user.btcAddress)
   // //console.log("props", props);
 
   // //console.log("state", props.location.state);
@@ -33,6 +43,26 @@ const Confirm = (props) => {
   //console.log(props.location);
 
   // const setShowModal = (props) => {};
+
+
+const handleCopy = () => {
+  setisCopied(true);
+  /* Get the text field */
+  // var copyText = document.querySelector("#image");
+  // copyText.select();
+  // document.execCommand("copy");
+
+  /* Select the text field */
+  // copyText.select();
+  // copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  // navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  // alert("Copied the text: " + copyText.value);
+}
+
 
   const handleConfirm = () => {
     // const fd = new FormData();
@@ -114,11 +144,11 @@ const Confirm = (props) => {
       <div className="container">
         <div className="row">
           <div
-            className="card mt-5 d-block mx-auto ConfirmCard"
+            className="card mt-5 mb-5 d-block mx-auto ConfirmCard"
             style={{
               borderRadius: 7,
               borderColor: "#0e73bc",
-              width: "70%",
+              width: "60%",
               padding: "25px",
               boxShadow: "1px 2px 5px",
             }}
@@ -167,7 +197,87 @@ const Confirm = (props) => {
               >
                 {error}
               </p>
-              <div className="table-responsive">
+
+{/* 
+              <button type="button" className="btn" data-toggle="tooltip" data-placement="bottom" title="Tooltip on bottom"><img src={copy1} alt="test" /></button> */}
+
+
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className="form-label mt-3"
+                  style={{ color: "black" }}
+                >
+                  Your Bitcoin Address
+                </label>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="btcAddress"
+                    value={user.btcAddress}
+                    disabled={true}
+                  />
+                  <CopyToClipboard onCopy={()=>setisCopied(true)} text={user.btcAddress}>
+                  
+                 
+                  
+
+
+                  {isCopied ?  
+                  <input
+                    type="image"
+                    src={copy1}
+                    id="copy1"
+                    width="48"
+                    style={{
+                      border: "1px solid #0E73BC",
+                      padding: 10,
+                      borderRadius: "0px 4px 4px 0px",
+                    }}
+                    alt="copy_icon"
+                    data-toggle="tooltip" data-placement="bottom" title="Copied"
+                  /> 
+                  :
+                   <input
+                  type="image"
+                  src={copy}
+                  id="copy"
+                  width="48"
+                  style={{
+                    border: "1px solid #0E73BC",
+                    padding: 10,
+                    borderRadius: "0px 4px 4px 0px",
+                  }}
+                  alt="copied_icon"
+                  data-toggle="tooltip" data-placement="bottom" title="Copy Address"
+                  onClick={handleCopy}
+                /> }
+                  
+                  
+                  
+                  
+                  {/* <input
+                    type="image"
+                    src={isCopied? copy1 : copy}
+                    id="image"
+                    width="48"
+                    style={{
+                      border: "1px solid #0E73BC",
+                      padding: 10,
+                      borderRadius: "0px 4px 4px 0px",
+                    }}
+                    alt="copy_icon"
+                    data-toggle="tooltip" data-placement="bottom" title={isCopied?"Copied":"Copy Address"}
+                    onClick={handleCopy}
+                  /> */}
+                  </CopyToClipboard>
+                  
+                  </div>
+                  <p className="text-center pt-5" style={{fontSize: "large", fontWeight: "500"}}>Your Payment Address QR Code</p>
+                  <div className="text-center pb-5"><QRCode id="qrStyle" size={150} value={user.btcAddress} /></div>
+
+
+              <div className="table-responsive" style={{height:300}}>
                 <table className="table table-bordered mt-4 mb-3">
                   <thead>
                     <tr>
